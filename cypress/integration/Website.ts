cae5b0d5dd7a3317516d90ff38bd719019ffb3b1
Rubén <ruben@swingby.network>
2020-11-12 09:58:51 +0100

    test: Mocked timestamps

--- cypress/integration/Website.ts
+++ cypress/integration/Website.ts
@@ -1,10 +1,15 @@
+import { DateTime } from 'luxon';
+
 describe('Website', () => {
   it('render correctly', () => {
     cy.viewport(1400, 1400);
 
     cy.server();
     cy.route('**/swaps/query', 'fixture:tx_mock_data.json');
+
+    cy.clock(DateTime.fromISO('2020-11-11T10:00:00.000Z').toJSDate());
     cy.visit('/');
+    cy.tick(1000);
 
     cy.get('[data-testid="main.loading-container"]').should('not.exist');
     cy.percySnapshot('Website', { widths: [1400] });
