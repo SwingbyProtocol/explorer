d3055a50140c29f7a43fa39001dd9ff1bd275c0b
roy1210 <rswkure@gmail.com>
2020-11-14 14:51:47 +0800

    feat: fetch 'float balance' and 'stats info' concurrently

--- src/modules/scenes/Main/Explorer/Browser/index.tsx
+++ src/modules/scenes/Main/Explorer/Browser/index.tsx
@@ -9,20 +9,29 @@ import { TxHistories } from '../TxHistories';
 import { Bottom, BrowserContainer, BrowserDiv, Top } from './styled';
 
 export const Browser = () => {
-  const [floatBalances, setFloatBalances] = useState({ btc: '0', btcb: '0', bnb: '0' });
+  const initialState = {
+    floatBalances: { btc: '0', btcb: '0', bnb: '0' },
+    stats: {
+      volume24Hr: '0',
+      rewards24Hr: 0,
+      volumes: ['1', '1', '1', '1', '1', '1', '1'],
+      validators: 0,
+    },
+  };
+
+  const [floatBalances, setFloatBalances] = useState(initialState.floatBalances);
   const [capacity, setCapacity] = useState(null);
-  const [stats, setStats] = useState({
-    volume24Hr: '0',
-    rewards24Hr: 0,
-    volumes: ['1', '1', '1', '1', '1', '1', '1'],
-    validators: 0,
-  });
+  const [stats, setStats] = useState(initialState.stats);
+
   useEffect(() => {
     (async () => {
-      const data = await fetchFloatBalances();
+      const results = await Promise.all([fetchFloatBalances(), fetchStatsInfo()]);
+
+      const data = results[0];
       setFloatBalances(data.floats);
       setCapacity(data.capacity);
-      const stats = await fetchStatsInfo();
+
+      const stats = results[1];
       setStats({
         volume24Hr: stats.volume24Hr,
         rewards24Hr: stats.rewards24Hr,
