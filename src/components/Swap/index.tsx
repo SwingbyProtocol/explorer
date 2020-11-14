0ba9918fc12b97eb91cc20b384f449f2ad65247f
roy1210 <rswkure@gmail.com>
2020-11-14 19:18:50 +0800

    styles: mobile screen for 'Swap'

--- src/components/Swap/index.tsx
+++ src/components/Swap/index.tsx
@@ -1,7 +1,19 @@
+import { Button } from '@swingby-protocol/pulsar';
+
 import { swapUrl } from '../../modules/env';
 
-import { StyledSwap } from './styled';
+import { StyledSwap, SwapMobileRow } from './styled';
 
 export const Swap = () => {
-  return <StyledSwap as="iframe" title="Swap" src={swapUrl} />;
+  return (
+    <>
+      <SwapMobileRow>
+        <Button variant="primary" size="state">
+          Swap
+        </Button>
+      </SwapMobileRow>
+
+      <StyledSwap as="iframe" title="Swap" src={swapUrl} />
+    </>
+  );
 };
--- src/components/Swap/styled.tsx
+++ src/components/Swap/styled.tsx
@@ -1,12 +1,29 @@
-import styled from 'styled-components';
 import { rem } from 'polished';
+import styled from 'styled-components';
 
+import { StylingConstants } from '../../modules/styles';
+
+const { media } = StylingConstants;
 export const StyledSwap = styled.iframe`
-  outline: none;
-  border: none;
-  margin: 0;
-  padding: 0;
-  display: block;
+  display: none;
+  @media (min-width: ${rem(media.sm)}) {
+    outline: none;
+    border: none;
+    margin: 0;
+    padding: 0;
+    display: block;
+    width: 100%;
+    height: ${({ theme }) => rem(theme.pulsar.size.state + theme.pulsar.size.street * 2)};
+  }
+`;
+
+export const SwapMobileRow = styled.div`
   width: 100%;
-  height: ${({ theme }) => rem(theme.pulsar.size.state + theme.pulsar.size.street * 2)};
+  padding-top: ${({ theme }) => rem(theme.pulsar.size.house)};
+  padding-bottom: ${({ theme }) => rem(theme.pulsar.size.house)};
+  padding-left: ${({ theme }) => rem(theme.pulsar.size.house)};
+  padding-right: ${({ theme }) => rem(theme.pulsar.size.house)};
+  @media (min-width: ${rem(media.sm)}) {
+    display: none;
+  }
 `;
