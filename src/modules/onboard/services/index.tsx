8d6465737bd60c499a828f6fe0eac97a5aea232b
roy1210 <rswkure@gmail.com>
2020-11-28 11:11:19 +0800

    debug: change API key

--- src/modules/onboard/services/index.tsx
+++ src/modules/onboard/services/index.tsx
@@ -3,7 +3,8 @@ import Onboard from 'bnc-onboard';
 import { BLOCK_NATIVE_API_KEY, GOERLI_NETWORK } from '../../env';
 
 const networkId = GOERLI_NETWORK;
-const dappId = BLOCK_NATIVE_API_KEY;
+// const dappId = BLOCK_NATIVE_API_KEY;
+const dappId = '9ed4d1ba-367b-48e6-b9bc-77e00782e4ba';
 
 // Ref: https://github.com/blocknative/react-demo/blob/master/src/services.js
 export const initOnboard = (subscriptions) => {
