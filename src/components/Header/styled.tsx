e0265583e4495302f6d9883ed4a3e6262020e835
roy1210 <rswkure@gmail.com>
2020-12-22 19:57:19 +0800

    styles: Update Header styling

--- src/components/Header/styled.tsx
+++ src/components/Header/styled.tsx
@@ -14,10 +14,10 @@ export const HeaderContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-top: ${({ theme }) => rem(theme.pulsar.size.street)};
+  padding-top: ${({ theme }) => rem(theme.pulsar.size.room)};
+  padding-bottom: ${({ theme }) => rem(theme.pulsar.size.room)};
   padding-right: ${({ theme }) => rem(theme.pulsar.size.house)};
   padding-left: ${({ theme }) => rem(theme.pulsar.size.house)};
-  padding-bottom: ${({ theme }) => rem(theme.pulsar.size.street)};
   border-bottom: 1px solid ${({ theme }) => theme.pulsar.color.border.normal};
   width: 100%;
   position: fixed;
@@ -51,7 +51,7 @@ export const Right = styled.div`
 export const Menu = styled.div`
   display: none;
   @media (min-width: ${rem(media.sm)}) {
-    width: ${rem(130)};
+    width: ${rem(140)};
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -63,7 +63,7 @@ export const Menu = styled.div`
 
 export const ThemeToggle = styled.div`
   margin-top: ${({ theme }) => rem(theme.pulsar.size.box)};
-  margin-right: ${({ theme }) => rem(theme.pulsar.size.street)};
+  margin-right: ${({ theme }) => rem(theme.pulsar.size.city)};
 `;
 
 export const MenuSpan = styled(Text)<MenuProps>`
@@ -122,4 +122,8 @@ export const ColumnPool = styled.div`
 export const IconLive = styled(StatusIcon)`
   font-size: ${({ theme }) => rem(theme.pulsar.size.box)};
   margin-left: ${({ theme }) => rem(theme.pulsar.size.drawer)};
+`;
+
+export const TextLang = styled(Text)`
+  font-size: ${({ theme }) => rem(theme.pulsar.size.closet)};
 `;
