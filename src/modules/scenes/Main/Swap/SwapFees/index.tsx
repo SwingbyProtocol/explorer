a07cc06fe5b3ffb5371029c2305bdf5d2b3a5aeb
roy1210 <rswkure@gmail.com>
2020-12-24 20:10:14 +0800

    fix: Swap fee calculation

--- src/modules/scenes/Main/Swap/SwapFees/index.tsx
+++ src/modules/scenes/Main/Swap/SwapFees/index.tsx
@@ -15,7 +15,8 @@ export const SwapFees = (props: Props) => {
   const { tx } = props;
   const explorer = useSelector((state) => state.explorer);
   const { transactionFees } = explorer;
-  const calculatedFees = transactionFees && calculateFixedFee(tx.feeCurrency, transactionFees);
+  // Memo: Advice from Luke:  Use the fee for the currencyOut, but the symbol is the feeCurrency
+  const calculatedFees = transactionFees && calculateFixedFee(tx.currencyOut, transactionFees);
   return (
     <SwapFeesContainer>
       <TitleText variant="accent">
@@ -27,8 +28,9 @@ export const SwapFees = (props: Props) => {
           <Text variant="accent"> {tx.fee}</Text>
         </CoinContainer>
         <Text variant="masked">
-          {calculatedFees && calculatedFees.feePercent}% +{' '}
+          {calculatedFees && calculatedFees.feePercent}% +
           {calculatedFees && calculatedFees.fixedFee} {tx.feeCurrency}
+          {/* Memo: Symbol is feeCurrency */}
         </Text>
       </Row>
     </SwapFeesContainer>
