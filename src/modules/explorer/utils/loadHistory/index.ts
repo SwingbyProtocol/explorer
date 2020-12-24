785fa14497e32348aaa46e5c99648e56f9aab896
roy1210 <rswkure@gmail.com>
2020-12-24 18:13:02 +0800

    fix: Use 'tx.feeCurrency' to show the fee currency icon

--- src/modules/explorer/utils/loadHistory/index.ts
+++ src/modules/explorer/utils/loadHistory/index.ts
@@ -168,7 +168,7 @@ const loadHistoryFiltered = async (args: IloadHistoryArgs): Promise<ITransaction
 
 export const loadHistory = async (arg: ILoadHistoryArgs): Promise<ILoadHistory> => {
   const { page, query, hash, isHideWaiting, bridge, prevTxsWithPage, swapHistoryTemp } = arg;
-
+  console.log('bridge', bridge);
   let tempMixedHistories = [];
   let txsWithPage: ITransactions = {
     data: {},
@@ -263,7 +263,6 @@ export const loadHistory = async (arg: ILoadHistoryArgs): Promise<ILoadHistory> 
       }
       // Memo: Default bridge as Ethereum Bridge
     } else if (bridge === '') {
-      console.log('hi');
       txsWithPage = await loadHistoryFiltered({
         page,
         query,
