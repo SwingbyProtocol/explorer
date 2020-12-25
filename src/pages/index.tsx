8435e7e917eb46bab39cd1f6f77ab3a3ff1641f1
roy1210 <rswkure@gmail.com>
2020-12-25 17:15:32 +0800

    wip: Deploy test with getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,13 @@
 import { getIpInfo, shouldBlockRegion } from '@swingby-protocol/ip-check';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import { NoServiceToUSModal } from '../components/NoServiceToUSModal';
 import { IPSTACK_API_KEY } from '../modules/env';
-import { fetch } from '../modules/fetch';
 import { Main } from '../modules/scenes';
 
-export default function Home() {
-  const [isNoServiceToUSModalOpen, setIsNoServiceToUSModalOpen] = useState(null);
-  const getIpAddress = async () => {
-    // Memo: Free plan => Can use 1K requests per day`
-    const url = 'https://ipapi.co/json/';
-    const result = await fetch<{ ip: string }>(url);
-    const ipAddress = result.ok && result.response.ip;
-    return ipAddress;
-  };
-
-  useEffect(() => {
-    (async () => {
-      const ipInfo = await (async () => {
-        try {
-          return await getIpInfo({
-            ip: await getIpAddress(),
-            ipstackApiKey: IPSTACK_API_KEY,
-          });
-        } catch (e) {
-          return e.message;
-        }
-      })();
-      setIsNoServiceToUSModalOpen(shouldBlockRegion(ipInfo));
-    })();
-  }, []);
+export default function Home({ ipInfo }) {
+  const [isNoServiceToUSModalOpen, setIsNoServiceToUSModalOpen] = useState(ipInfo.blockRegion);
+  console.log('ipInfo', ipInfo);
 
   return (
     <>
@@ -42,3 +19,59 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps = async ({ req }) => {
+  const clientIp =
+    (typeof req.headers['x-real-ip'] === 'string' ? req.headers['x-real-ip'] : null) ??
+    req.connection.remoteAddress ??
+    null;
+
+  const ipInfo = await (async () => {
+    try {
+      if (!clientIp || !process.env.IPSTACK_API_KEY) return null;
+      return await getIpInfo({
+        ip: clientIp,
+        ipstackApiKey: IPSTACK_API_KEY ?? '',
+      });
+    } catch (e) {
+      return null;
+    }
+  })();
+
+  const blockRegion = (() => {
+    try {
+      if (!ipInfo) return false;
+      return shouldBlockRegion(ipInfo);
+    } catch (e) {
+      return false;
+    }
+  })();
+
+  return { props: { ipInfo: { ipInfo, clientIp, blockRegion } } };
+};
+
+// Memo: Legacy code. Will remove after test
+
+// const getIpAddress = async () => {
+//   // Memo: Free plan => Can use 1K requests per day
+//   const url = 'https://ipapi.co/json/';
+//   const result = await fetch<{ ip: string }>(url);
+//   const ipAddress = result.ok && result.response.ip;
+//   return ipAddress;
+// };
+
+// useEffect(() => {
+//   (async () => {
+//     const ipInfo = await (async () => {
+//       try {
+//         return await getIpInfo({
+//           ip: await getIpAddress(),
+//           ipstackApiKey: IPSTACK_API_KEY,
+//         });
+//       } catch (e) {
+//         return e.message;
+//       }
+//     })();
+//     setIsNoServiceToUSModalOpen(shouldBlockRegion(ipInfo));
+//   })();
+// }, []);
