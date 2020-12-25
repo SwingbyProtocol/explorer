30f78698f4c21b2f034f317abbb3bb0517fb03d2
roy1210 <rswkure@gmail.com>
2020-12-25 11:10:20 +0800

    wip: Try deploy with remove getInitialProps function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,9 @@ import { NoServiceToUSModal } from '../components/NoServiceToUSModal';
 import { IPSTACK_API_KEY } from '../modules/env';
 import { Main } from '../modules/scenes';
 
-export default function Home({ blockRegion, clientIp }) {
-  const [isNoServiceToUSModalOpen, setIsNoServiceToUSModalOpen] = useState(blockRegion);
+export default function Home() {
+  const [isShouldBlockRegion, setIsShouldBlockRegion] = useState(null);
+  const [isNoServiceToUSModalOpen, setIsNoServiceToUSModalOpen] = useState(null);
   const [location, setLocation] = useState(null);
   useEffect(() => {
     fetch('https://ipapi.co/json/')
@@ -14,9 +15,24 @@ export default function Home({ blockRegion, clientIp }) {
       .then((data) => setLocation(data));
   }, []);
 
-  console.log('location', location);
-  console.log('blockRegion', blockRegion);
-  console.log('clientIp', clientIp);
+  useEffect(() => {
+    (async () => {
+      const calilfolniaIp = '192.119.164.87';
+
+      const ipInfo = await (async () => {
+        try {
+          return await getIpInfo({
+            // ip: clientIp,
+            ip: calilfolniaIp,
+            ipstackApiKey: IPSTACK_API_KEY,
+          });
+        } catch (e) {
+          return e.message;
+        }
+      })();
+      setIsNoServiceToUSModalOpen(shouldBlockRegion(ipInfo as any));
+    })();
+  }, []);
 
   return (
     <>
@@ -27,36 +43,4 @@ export default function Home({ blockRegion, clientIp }) {
       <Main />
     </>
   );
-}
-
-export async function getServerSideProps({ req }) {
-  const clientIp =
-    (typeof req.headers['x-real-ip'] === 'string' ? req.headers['x-real-ip'] : undefined) ??
-    req.connection.remoteAddress ??
-    '';
-  const calilfolniaIp = '192.119.164.87';
-
-  const ipInfo = await (async () => {
-    try {
-      return await getIpInfo({
-        // ip: clientIp,
-        ip: calilfolniaIp,
-        ipstackApiKey: IPSTACK_API_KEY,
-      });
-    } catch (e) {
-      return e.message;
-    }
-  })();
-
-  const blockRegion = (() => {
-    try {
-      return shouldBlockRegion(ipInfo as any);
-    } catch (e) {
-      return false;
-    }
-  })();
-
-  return {
-    props: { blockRegion, clientIp },
-  };
 }
