eba30d6059bc8663bd7f1af15fb7d73d46b31874
roy1210 <rswkure@gmail.com>
2020-12-25 10:46:53 +0800

    wip: try deploy with hard code IP adress as Califolnia

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import { getIpInfo, shouldBlockRegion } from '@swingby-protocol/ip-check';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import { NoServiceToUSModal } from '../components/NoServiceToUSModal';
 import { IPSTACK_API_KEY } from '../modules/env';
 import { Main } from '../modules/scenes';
 
-export default function Home({ blockRegion }) {
+export default function Home({ blockRegion, clientIp }) {
   const [isNoServiceToUSModalOpen, setIsNoServiceToUSModalOpen] = useState(blockRegion);
+  const [location, setLocation] = useState(null);
+  useEffect(() => {
+    fetch('https://ipapi.co/json/')
+      .then((response) => response.json())
+      .then((data) => setLocation(data));
+  }, []);
+
+  console.log('location', location);
+  console.log('blockRegion', blockRegion);
+  console.log('clientIp', clientIp);
+
   return (
     <>
       <NoServiceToUSModal
@@ -23,12 +34,14 @@ export async function getServerSideProps({ req }) {
     (typeof req.headers['x-real-ip'] === 'string' ? req.headers['x-real-ip'] : undefined) ??
     req.connection.remoteAddress ??
     '';
+  const calilfolniaIp = '192.119.164.87';
 
   const ipInfo = await (async () => {
     try {
       return await getIpInfo({
-        ip: clientIp,
-        ipstackApiKey: 'b74a888cf8d2112ef1a0a72367e4c696',
+        // ip: clientIp,
+        ip: calilfolniaIp,
+        ipstackApiKey: IPSTACK_API_KEY,
       });
     } catch (e) {
       return e.message;
@@ -44,6 +57,6 @@ export async function getServerSideProps({ req }) {
   })();
 
   return {
-    props: { blockRegion },
+    props: { blockRegion, clientIp },
   };
 }
