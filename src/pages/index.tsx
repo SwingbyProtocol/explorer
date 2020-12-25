d9901aaa7637c80f30170656deec839edcb3a607
roy1210 <rswkure@gmail.com>
2020-12-25 11:33:00 +0800

    wip: Deploy test without CorsAnywhere

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,34 +3,32 @@ import { useEffect, useState } from 'react';
 
 import { NoServiceToUSModal } from '../components/NoServiceToUSModal';
 import { IPSTACK_API_KEY } from '../modules/env';
+import { fetch } from '../modules/fetch';
 import { Main } from '../modules/scenes';
 
 export default function Home() {
-  const [isShouldBlockRegion, setIsShouldBlockRegion] = useState(null);
   const [isNoServiceToUSModalOpen, setIsNoServiceToUSModalOpen] = useState(null);
-  const [location, setLocation] = useState(null);
-  useEffect(() => {
-    fetch('https://ipapi.co/json/')
-      .then((response) => response.json())
-      .then((data) => setLocation(data));
-  }, []);
+  const getIpAddress = async () => {
+    const url = `http://api.ipstack.com/check?access_key=${IPSTACK_API_KEY}`;
+    const result = await fetch<{ ip: string }>(url);
+    const ipAddress = result.ok && result.response.ip;
+    console.log('ipAddress', ipAddress);
+    return ipAddress;
+  };
 
   useEffect(() => {
     (async () => {
-      const calilfolniaIp = '192.119.164.87';
-
       const ipInfo = await (async () => {
         try {
           return await getIpInfo({
-            // ip: clientIp,
-            ip: calilfolniaIp,
+            ip: await getIpAddress(),
             ipstackApiKey: IPSTACK_API_KEY,
           });
         } catch (e) {
           return e.message;
         }
       })();
-      setIsNoServiceToUSModalOpen(shouldBlockRegion(ipInfo as any));
+      setIsNoServiceToUSModalOpen(shouldBlockRegion(ipInfo));
     })();
   }, []);
 
