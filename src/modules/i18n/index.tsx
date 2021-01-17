a2fd34286145c99e8f9c4bed9953f4eb58c7f62a
Shoe Kure <rswkure@gmail.com>
2021-01-17 18:44:27 +0800

    feat: add Japanese (#68)
    * feat: add Japanese
    
    * fix: update translation
    
    * fix: run format

--- src/modules/i18n/index.tsx
+++ src/modules/i18n/index.tsx
@@ -3,11 +3,13 @@
 import en from './files/en.json';
 import zh from './files/zh.json';
 import zh_TW from './files/zh-TW.json';
+import ja from './files/ja.json';
 
 export const languages = {
   en,
   zh,
   'zh-TW': zh_TW,
+  ja,
 };
 
 export const languagesSelector = [
@@ -22,5 +24,9 @@ export const languagesSelector = [
   {
     text: 'Chinese (Traditional)',
     code: 'zh-TW',
+  },
+  {
+    text: 'Japanese',
+    code: 'ja',
   },
 ];
