93c975111bb392a83a836a0a416d80835fc805f0
roy1210 <rswkure@gmail.com>
2021-03-31 12:09:16 +0800

    feat: add Transaction type filter

--- src/modules/scenes/Main/Explorer/TxHistories/index.tsx
+++ src/modules/scenes/Main/Explorer/TxHistories/index.tsx
@@ -1,6 +1,6 @@
 import { Dropdown, Text } from '@swingby-protocol/pulsar';
 import { useRouter } from 'next/router';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 import AutoSizer from 'react-virtualized-auto-sizer';
@@ -8,9 +8,9 @@ import { FixedSizeList as List } from 'react-window';
 import InfiniteLoader from 'react-window-infinite-loader';
 
 import { Loader } from '../../../../../components/Loader';
-import { TransactionType } from '../../../../../generated/graphql';
+import { Bridge, TransactionType } from '../../../../../generated/graphql';
 import { isEnableBscSupport, TXS_COUNT } from '../../../../env';
-import { selectableBridge } from '../../../../explorer';
+import { ISwapQueryPrams, selectableBridge, selectableTxType } from '../../../../explorer';
 import { useLoadHistories } from '../../../../hooks';
 import { toggleIsRejectedTx } from '../../../../store';
 
@@ -34,16 +34,19 @@ export const TxHistories = () => {
   const params = query;
 
   const q = String(params.q || '');
-  const chainBridge = String(params.bridge || '');
+  const chainBridge = String(params.bridge || '') as Bridge;
+  const type = String(params.type || '') as TransactionType;
 
   const routerPush = useCallback(
-    (bridge: string, q: string): void => {
+    (params: ISwapQueryPrams): void => {
+      const { bridge, type, q } = params;
+
       // Memo: Shallow routing make URL faster update and page won't get replaced. Only the state of the route is changed.
       // Ref: https://nextjs.org/docs/routing/shallow-routing
       push(
         {
           pathname: '/',
-          query: { bridge, q },
+          query: { bridge, type, q },
         },
         undefined,
         { shallow: true },
@@ -56,43 +59,50 @@ export const TxHistories = () => {
   const explorer = useSelector((state) => state.explorer);
   const { isRejectedTx } = explorer;
 
-  const [filterTransaction, setFilterTransaction] = useState<TransactionType>(TransactionType.Swap);
+  const { data, loading, fetchMoreQuery } = useLoadHistories();
 
-  const { data, loading, fetchMoreQuery } = useLoadHistories(filterTransaction);
-
-  const filter = (
-    <Dropdown target={<Filter />}>
+  const statusFilter = (
+    <>
       {/* Memo: Just shows user the filter is hiding 'waiting' */}
       <Dropdown.Item selected={true} disabled={true}>
-        <FormattedMessage id="home.recent-swaps.hide-waiting" />
+        <FormattedMessage id="home.recent-swaps.filter.hide-waiting" />
       </Dropdown.Item>
       <Dropdown.Item
         selected={isRejectedTx}
         onClick={() => {
-          routerPush(chainBridge, q);
+          routerPush({ bridge: chainBridge, q, type });
           dispatch(toggleIsRejectedTx(!isRejectedTx));
         }}
       >
-        <FormattedMessage id="home.recent-swaps.rejected-tx" />
+        <FormattedMessage id="home.recent-swaps.filter.rejected-tx" />
       </Dropdown.Item>
-      <Dropdown.Item
-        selected={filterTransaction === TransactionType.Deposit}
-        onClick={() => {
-          if (filterTransaction === TransactionType.Deposit) {
-            setFilterTransaction(TransactionType.Swap);
-          } else {
-            setFilterTransaction(TransactionType.Deposit);
-          }
-        }}
-      >
-        <FormattedMessage id="home.recent-swaps.float-txs" />
-      </Dropdown.Item>
-      <Dropdown.Divider />
+    </>
+  );
+
+  const txTypeFilter = (
+    <>
+      {selectableTxType.map((txType) => {
+        return (
+          <Dropdown.Item
+            selected={type === txType.type}
+            onClick={() => {
+              routerPush({ bridge: chainBridge, type: txType.type, q });
+            }}
+          >
+            <FormattedMessage id={txType.menu} />
+          </Dropdown.Item>
+        );
+      })}
+    </>
+  );
+
+  const selectableBridgeFilter = (
+    <>
       {selectableBridge.map((chain) => {
         return (
           <Dropdown.Item
             selected={chainBridge === chain.bridge}
-            onClick={() => routerPush(chain.bridge, q)}
+            onClick={() => routerPush({ bridge: chain.bridge, type, q })}
             key={chain.menu}
             disabled={!isEnableBscSupport && chain.menu === 'BSC'}
           >
@@ -103,6 +113,16 @@ export const TxHistories = () => {
           </Dropdown.Item>
         );
       })}
+    </>
+  );
+
+  const filter = (
+    <Dropdown target={<Filter />}>
+      {selectableBridgeFilter}
+      <Dropdown.Divider />
+      {txTypeFilter}
+      <Dropdown.Divider />
+      {statusFilter}
     </Dropdown>
   );
 
@@ -121,6 +141,8 @@ export const TxHistories = () => {
     </NoResultsFound>
   );
 
+  const isFloatTx = type === TransactionType.Deposit || type === TransactionType.Withdrawal;
+
   return (
     <>
       <TxHistoriesContainer txsHeight={TABLE_ROW_COUNT * ROW_HEIGHT}>
@@ -130,9 +152,9 @@ export const TxHistories = () => {
               <FormattedMessage id="home.recent-swaps.recent-swaps" />
             </Text>
           </Left>
-          <Right isFloats={chainBridge === 'floats'}>
+          <Right isFloats={isFloatTx}>
             <TextFee variant="section-title">
-              {chainBridge === 'floats' ? (
+              {isFloatTx ? (
                 <FormattedMessage id="home.recent-swaps.fees-max" />
               ) : (
                 <FormattedMessage id="home.recent-swaps.fees" />
