f786ccb4514ff598f6ae67e9fcbb63d1f5f9d272
tarrio <73276750+tarrio@users.noreply.github.com>
2021-10-14 16:57:43 +0200

    feat: New header (#198)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,8 @@
 import { SwingbyHeader, LocaleSwitcher, ThemeSwitcher } from '@swingby-protocol/header';
 import { useRouter } from 'next/router';
-import React, { useCallback, useMemo } from 'react';
+import React, { useCallback } from 'react';
 import { useIntl } from 'react-intl';
 
-import { PATH } from '../../modules/env';
-import { URL } from '../../modules/links';
 import { useThemeSettings } from '../../modules/store/settings';
 
 import { HeaderContainer } from './styled';
@@ -14,25 +12,6 @@ export const Header = () => {
   const { locale } = useIntl();
   const [theme, setTheme] = useThemeSettings();
 
-  const items = useMemo(
-    () => [
-      {
-        key: 'farms',
-        render: 'Yield Farming',
-        href: URL.YieldFarming,
-        target: '_blank',
-        rel: 'noopener noreferrer',
-      },
-      {
-        key: 'stake',
-        render: 'Stake and Earn BTC',
-        href: `/${locale}${PATH.POOL}/?bridge=btc_erc`,
-      },
-      { key: 'metanodes', render: 'Metanodes', href: `/${locale}${PATH.METANODES}` },
-    ],
-    [locale],
-  );
-
   const changeLocale = useCallback((locale: string) => push(asPath, null, { locale }), [
     push,
     asPath,
@@ -41,7 +20,6 @@ export const Header = () => {
   return (
     <HeaderContainer>
       <SwingbyHeader
-        productName="Explorer"
         logoHref={`/${locale}`}
         barItems={
           <>
@@ -49,7 +27,6 @@ export const Header = () => {
             <LocaleSwitcher locale={locale} locales={locales} onChange={changeLocale} />
           </>
         }
-        items={items}
       />
     </HeaderContainer>
   );
