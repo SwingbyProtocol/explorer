880ed944a7e6afbd15f74849a0edb47861cfacbf
Tarrio <73276750+tarrio@users.noreply.github.com>
2021-10-14 16:50:39 +0200

    fix: Remove product name

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -20,7 +20,6 @@ export const Header = () => {
   return (
     <HeaderContainer>
       <SwingbyHeader
-        productName="Explorer"
         logoHref={`/${locale}`}
         barItems={
           <>
