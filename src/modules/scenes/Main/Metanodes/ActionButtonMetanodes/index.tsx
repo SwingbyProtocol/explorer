8e19dfefae60670d7cf93845ce44202d4f4588a4
Roy <rswkure@gmail.com>
2021-11-15 17:06:12 +0800

    feat: included '+ swap fee share' word next to APR in /metanodes (#209)

--- src/modules/scenes/Main/Metanodes/ActionButtonMetanodes/index.tsx
+++ src/modules/scenes/Main/Metanodes/ActionButtonMetanodes/index.tsx
@@ -1,3 +1,4 @@
+import { Tooltip, Text } from '@swingby-protocol/pulsar';
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
@@ -9,6 +10,8 @@ import {
   RowText,
   TextAPR,
   TextTitle,
+  TextSwapFee,
+  InfoIcon,
 } from './styled';
 
 export const ActionButtonMetanodes = () => {
@@ -16,12 +19,31 @@ export const ActionButtonMetanodes = () => {
   return (
     <ActionButtonMetanodesContainer>
       <RowText>
-        <TextTitle variant="accent">
-          <FormattedMessage id="pool.apr" />
-        </TextTitle>
-        <TextAPR variant="accent">
-          <FormattedMessage id="common.percent" values={{ value: ' ' + apr }} />
-        </TextAPR>
+        <div>
+          <TextTitle variant="accent">
+            <FormattedMessage id="pool.apr" />
+          </TextTitle>
+          <TextAPR variant="accent">
+            <FormattedMessage id="common.percent" values={{ value: ' ' + apr }} />
+          </TextAPR>
+        </div>
+        <div>
+          <TextSwapFee>
+            <FormattedMessage id="metanodes.swap-fee" />
+          </TextSwapFee>
+          <Tooltip
+            content={
+              <Tooltip.Content>
+                <Text variant="normal">
+                  <FormattedMessage id="metanodes.swap-fee.detail" />
+                </Text>
+              </Tooltip.Content>
+            }
+            targetHtmlTag="span"
+          >
+            <InfoIcon />
+          </Tooltip>
+        </div>
       </RowText>
       <ButtonContainer>
         <RewardButton />
--- src/modules/scenes/Main/Metanodes/ActionButtonMetanodes/styled.tsx
+++ src/modules/scenes/Main/Metanodes/ActionButtonMetanodes/styled.tsx
@@ -3,6 +3,7 @@ import { rem } from 'polished';
 import styled from 'styled-components';
 
 import { StylingConstants } from '../../../../styles';
+import { IconInfo } from '../../../Common';
 
 const { media } = StylingConstants;
 
@@ -11,23 +12,26 @@ export const ActionButtonMetanodesContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-bottom: ${({ theme }) => rem(theme.pulsar.size.street)};
+  padding-left: ${({ theme }) => rem(theme.pulsar.size.box)};
+  padding-right: ${({ theme }) => rem(theme.pulsar.size.box)};
+  @media (min-width: ${rem(media.sm)}) {
+    padding-left: 0;
+    padding-right: 0;
+  }
 `;
 
 export const RowText = styled.div`
-  width: ${rem(80)};
+  display: flex;
+  flex-direction: column;
   @media (min-width: ${rem(media.sm)}) {
+    align-items: center;
+    flex-direction: row;
     width: auto;
+    column-gap: ${({ theme }) => rem(theme.pulsar.size.box)};
   }
 `;
 
-export const ButtonContainer = styled.div`
-  display: grid;
-  grid-template-columns: auto auto;
-  grid-column-gap: ${({ theme }) => rem(theme.pulsar.size.drawer)};
-  @media (min-width: ${rem(media.sm)}) {
-    grid-column-gap: ${({ theme }) => rem(theme.pulsar.size.house)};
-  }
-`;
+export const ButtonContainer = styled.div``;
 
 export const TextTitle = styled(Text)`
   font-size: ${({ theme }) => rem(theme.pulsar.size.house)};
@@ -36,4 +40,17 @@ export const TextTitle = styled(Text)`
 export const TextAPR = styled(Text)`
   font-size: ${({ theme }) => rem(theme.pulsar.size.house)};
   color: ${({ theme }) => theme.pulsar.color.primary.normal};
+`;
+
+export const TextSwapFee = styled(Text)`
+  font-size: ${({ theme }) => rem(theme.pulsar.size.room)};
+  @media (min-width: ${rem(media.lg)}) {
+    font-size: ${({ theme }) => rem(theme.pulsar.size.house)};
+  }
+`;
+
+export const InfoIcon = styled(IconInfo)`
+  font-size: ${({ theme }) => rem(theme.pulsar.size.closet)};
+  margin-bottom: ${rem(-1)};
+  margin-left: ${({ theme }) => rem(theme.pulsar.size.box)};
 `;
